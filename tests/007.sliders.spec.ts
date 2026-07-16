7db817fb1863e45add7a6cc2ddbb749bc7f665ea
Emma Popova <emma.p.popova@gmail.com>
2026-07-16 18:37:44 +0300

    practice slider interaction with mouse

--- tests/007.sliders.spec.ts
+++ tests/007.sliders.spec.ts
@@ -16,5 +16,39 @@ test.describe("Sliders",()=> {
         })
         await tempGauge.click();
     })
+        test("sliders- Mouse movement - 1", async({page})=>{
+        await page.getByText("IoT Dashboard").click();
+        
+        const tempBox = page.locator('[tabtitle="Temperature"] ngx-temperature-dragger');
+        await tempBox.scrollIntoViewIfNeeded();
+
+        const box = await tempBox.boundingBox();
+
+        const x = box!.x + box!.width / 2;
+         const y = box!.y + box!.height / 2;
+        await page.mouse.move(x, y);
+        await page.mouse.down();
+        await page.mouse.move(x+100, y);
+        await page.mouse.move(x + 100, y + 100);
+        await page.mouse.up();
+        await expect(tempBox).toContainText('30');
+    })
+            test("sliders- Mouse movement - 2", async({page})=>{
+        await page.getByText("IoT Dashboard").click();
+        
+        const tempBox = page.locator('[tabtitle="Temperature"] ngx-temperature-dragger');
+        await tempBox.scrollIntoViewIfNeeded();
+
+        const box = await tempBox.boundingBox();
+
+        const x = box!.x + box!.width / 2;
+         const y = box!.y + box!.height / 2;
+        await page.mouse.move(x, y);
+        await page.mouse.down();
+        await page.mouse.move(x -100, y);
+        await page.mouse.move(x -100, y +100);
+        await page.mouse.up();
+        await expect(tempBox).toContainText('13');
+    })
 
 })
